99582dcc54be88f038f0e6c5bb995f350bafd7d2
Marvin Siri <marvinsiri123@gmail.com>
2023-12-19 18:18:13 -0500

    pulled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
 // import React from 'react';
 import logo from './images/logo.png';
 
-
 function App() {
   return (
     <>
-    
       <header>
         <nav>
           <ul>
-            <li><a>Shop</a></li>
-            <li><img id="logo" src={logo} alt="Flux Logo" /></li>
+            <li>
+              <a>Shop</a>
+            </li>
+            <li>
+              <img id="logo" src={logo} alt="Flux Logo" />
+            </li>
           </ul>
         </nav>
       </header>
 
-      <section>
-
-      </section>
+      <section></section>
     </>
   );
 }
