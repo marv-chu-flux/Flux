7f286020b4747153181567fea7ff870ee847460b
Marvin Siri <marvinsiri123@gmail.com>
2023-12-19 16:01:08 -0500

    refreshed file structure, front end talking to back end

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3456;
+
+app.use(express.json());
+app.use(cors());
+
+//setting public files
+// const publicDir = path.join(__dirname, '../client');
+// const staticAssets = express.static(publicDir);
+// app.use(staticAssets);
+
+// app.use(routes);
+
+app.get('/api', (req, res) => {
+  res.send({ msg: 'hi' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is now running on http://localhost:${PORT}`);
+});
