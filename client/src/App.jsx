19662e6e524957bd46b132df93f0675f7316bb28
thisischu <chuyilei1@gmail.com>
2023-12-20 11:18:31 -0500

    navbar created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,18 @@
 import { Link } from 'react-router-dom';
 import logo from './images/logo.png';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import ItemCard from './components/ItemCard/Card';
+
 
 function App() {
   return (
     <>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to={"shop"}>Shop</Link>
-            </li>
-            <li>
-              <img id="logo" src={logo} alt="Flux Logo" />
-            </li>
-          </ul>
-        </nav>
-      </header>
-
+      <Header/>
+      {/* <ItemCard/> */}
+      <Footer/>
     </>
   );
 }
