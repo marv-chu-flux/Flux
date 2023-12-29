3c917e5a8954968dea512fb611803ff44f28832a
Marvin Siri <marvinsiri123@gmail.com>
2023-12-29 01:13:48 -0500

    delete all cart functionality

--- server/routes.js
+++ server/routes.js
@@ -9,5 +9,7 @@ router.use(addModels);
 router.get('/cart', controllers.list);
 router.post('/cart', controllers.create);
 router.delete('/cart:id', controllers.destroy);
+router.patch('/cart:id', controllers.update);
+router.delete('/cart', controllers.destroyAll);
 
 module.exports = router;
