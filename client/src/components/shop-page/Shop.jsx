bab09a468a0449beca3ca7f36652a81fb5ca732c
thisischu <chuyilei1@gmail.com>
2023-12-28 21:08:10 -0500

    search bar functionality + cart icon

--- client/src/components/shop-page/Shop.jsx
+++ client/src/components/shop-page/Shop.jsx
@@ -2,11 +2,13 @@ import Footer from '../reusableComponents/Footer/Footer';
 import Header from '../reusableComponents/Header/Header';
 import Products from './Products';
 import { Outlet } from 'react-router-dom';
+import SearchBar from './Searchbar/SearchBar';
 
 export default function Shop() {
   return (
     <>
       <Header />
+      <SearchBar/>
       <Products />
       <Footer />
       <div id="detail">
