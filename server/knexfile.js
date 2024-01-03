deb709129a970ffd8337387c5c01e133d57b2cf4
Marvin Siri <marvinsiri123@gmail.com>
2024-01-03 15:46:28 -0500

    fixing db connection string to connect remote db

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,11 +5,11 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.PG_HOST || '127.0.0.1',
-      port: process.env.PG_PORT || 5432,
-      user: process.env.PG_USER || 'postgres',
-      password: process.env.PG_PASSWORD || 'postgres',
-      database: process.env.PG_DATABASE || 'flux',
+      // host: process.env.PG_HOST || '127.0.0.1',
+      // user: process.env.PG_USER || 'postgres',
+      // password: process.env.PG_PASSWORD || 'postgres',
+      // database: process.env.PG_DATABASE || 'flux',
+      connectionString: process.env.PG_CONNECTION_STRING,
     },
     migrations: {
       directory: path.join(__dirname, 'db', 'migrations'),
@@ -22,16 +22,9 @@ module.exports = {
     client: 'pg',
     connection: {
       host: process.env.PG_HOST || '127.0.0.1',
-      port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
       password: process.env.PG_PASSWORD || 'postgres',
       database: process.env.PG_DATABASE || 'flux',
     },
   },
 };
-
-// host: process.env.PG_HOST || '127.0.0.1',
-// port: process.env.PG_PORT || 5432,
-// user: process.env.PG_USER || 'postgres',
-// password: process.env.PG_PASSWORD || 'postgres',
-// database: process.env.PG_DATABASE || 'postgres',
