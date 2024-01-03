6d820f9eb4c7f5589eb69fe52ca87925b6739bec
Marvin Siri <marvinsiri123@gmail.com>
2024-01-03 15:53:27 -0500

    attempting to deploy db

--- server/knexfile.js
+++ server/knexfile.js
@@ -21,10 +21,7 @@ module.exports = {
   test: {
     client: 'pg',
     connection: {
-      host: process.env.PG_HOST || '127.0.0.1',
-      user: process.env.PG_USER || 'postgres',
-      password: process.env.PG_PASSWORD || 'postgres',
-      database: process.env.PG_DATABASE || 'flux',
+      connectionString: process.env.PG_CONNECTION_STRING,
     },
   },
 };
