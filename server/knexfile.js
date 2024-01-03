536c65587a45e129ce4b299c13629bc1d0006aa8
Marvin Siri <marvinsiri123@gmail.com>
2024-01-03 15:15:15 -0500

    fixing db connection for deployment

--- server/knexfile.js
+++ server/knexfile.js
@@ -25,7 +25,13 @@ module.exports = {
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
       password: process.env.PG_PASSWORD || 'postgres',
-      database: process.env.PG_DATABASE || 'postgres',
+      database: process.env.PG_DATABASE || 'flux',
     },
   },
 };
+
+// host: process.env.PG_HOST || '127.0.0.1',
+// port: process.env.PG_PORT || 5432,
+// user: process.env.PG_USER || 'postgres',
+// password: process.env.PG_PASSWORD || 'postgres',
+// database: process.env.PG_DATABASE || 'postgres',
--- server/server.js
+++ server/server.js
@@ -1,22 +1,19 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
 const routes = require('./routes');
-
+require('dotenv').config();
 const app = express();
-const PORT = 3456;
+
+const PORT = process.env.PORT || 3456;
 
 app.use(express.json());
 app.use(cors());
 
-//setting public files
-// const publicDir = path.join(__dirname, '../client');
-// const staticAssets = express.static(publicDir);
-// app.use(staticAssets);
-
-// app.use(routes);
-
 app.use(routes);
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'SERVER IS UP' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is now running on http://localhost:${PORT}`);
