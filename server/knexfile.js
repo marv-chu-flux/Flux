ba8be98c6c0e26b6475dbe26b624ff4a4140f7ca
Marvin Siri <marvinsiri123@gmail.com>
2024-02-08 17:25:04 -0500

    fixx

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,9 +4,7 @@ const path = require('path');
 module.exports = {
   production: {
     client: 'pg',
-    connection: {
-      connectionString: process.env.PG_CONNECTION_STRING,
-    },
+    connection: process.env.PG_CONNECTION_STRING,
     migrations: {
       directory: path.join(__dirname, 'db', 'migrations'),
     },
